7d78aa52630e8555cc4924d505d010af88bf2277
dading <dgusti@student.unimelb.edu.au>
2019-10-28 11:46:36 +1100

    finalize UI testing

--- Parkiminder BetaUITests/Parkiminder_BetaUITests.swift
+++ Parkiminder BetaUITests/Parkiminder_BetaUITests.swift
@@ -14,32 +14,23 @@ class Parkiminder_BetaUITests: XCTestCase {
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
         
+        // stop immediately after failure
+        continueAfterFailure = false
         app = XCUIApplication()
         app.launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    
-    func inspect() {
-        
-    }
-    
-    
+    // test the CustomTimerView, make sure it switches mode correctly and dismissed if tapped outside
     func testCustomTimerView() {
-
-        // launch the application
-        let app = XCUIApplication()
         // since simulator doesn't have hardware camera, alert is shown, then dismiss is tapped
-        app.alerts["No Camera"].scrollViews.otherElements.buttons["Dismiss"].tap()
+        let nocameraAlert = app.alerts["No Camera"]
+        if nocameraAlert.exists {
+            nocameraAlert.scrollViews.otherElements.buttons["Dismiss"].tap()
+        }
         // tap custom timer button
         app.buttons["icon stopwatch"].tap()
         let setButton = app.buttons["SET"]
@@ -52,14 +43,16 @@ class Parkiminder_BetaUITests: XCTestCase {
         // tap Pick Date
         dateMode.tap()
         // assert picker mode is changed from timer to date
-        if dateMode.isSelected {
-            numberOfWheels = app.datePickers.pickerWheels.count
-            XCTAssertTrue(numberOfWheels == 3)
-        } else if timerMode.isSelected {
-            numberOfWheels = app.datePickers.pickerWheels.count
-            XCTAssertTrue(numberOfWheels == 2)
-        }
-        // any point outside the custom timer view
+        numberOfWheels = app.datePickers.pickerWheels.count
+        // picker has at least 3 components i.e. date, hour and minute
+        XCTAssertTrue(numberOfWheels >= 3)
+        // tap set timer
+        timerMode.tap()
+        // picker has 2 components i.e. hour and minute
+        numberOfWheels = app.datePickers.pickerWheels.count
+        XCTAssertTrue(numberOfWheels == 2)
+       
+        // random point x=40 y=40 outside the custom timer view
         let offPickerArea = app.children(matching: .window).element(boundBy: 0).coordinate(withNormalizedOffset: CGVector(dx: 40, dy: 40))
         // tap any point
         offPickerArea.tap()
@@ -68,25 +61,60 @@ class Parkiminder_BetaUITests: XCTestCase {
         
     }
     
-    func testSegueFromHome() {
+    // this function tests navigating from main View to CountDownView
+    func testCountDownView() {
+        // since simulator doesn't have hardware camera, alert is shown, then dismiss is tapped
+        let nocameraAlert = app.alerts["No Camera"]
+        if nocameraAlert.exists {
+            nocameraAlert.scrollViews.otherElements.buttons["Dismiss"].tap()
+        }
+        // tap 30 mins button
+        let thirtyMinsButton = app.collectionViews.staticTexts["30"]
+        thirtyMinsButton.tap()
+        let imhereButton = app.staticTexts["I'm Here"]
+        // test app navigate to CountDownView
+        XCTAssertTrue(imhereButton.exists)
+        imhereButton.tap()
+        // test app navigate back to main View
+        XCTAssertFalse(imhereButton.exists)
+        XCTAssertTrue(thirtyMinsButton.exists)
+        // navigate to HistoryView and make sure just created record exists
+        let historyButton = app.buttons["icon menu"]
+        historyButton.tap()
+        let createdRecord = app.tables.cells.staticTexts["30m"]
+        XCTAssertTrue(createdRecord.exists)
+    }
+    
+    // test navigate from main View to HistoryView
+    func testTapHistory() {
+        // since simulator doesn't have hardware camera, alert is shown, then dismiss is tapped
+        let nocameraAlert = app.alerts["No Camera"]
+        if nocameraAlert.exists {
+            nocameraAlert.scrollViews.otherElements.buttons["Dismiss"].tap()
+        }
         
-        let app = XCUIApplication()
-        app.alerts["No Camera"].scrollViews.otherElements.buttons["Dismiss"].tap()
-        app.buttons["icon menu"].tap()
+        let historyButton = app.buttons["icon menu"]
+        
+        historyButton.tap()
+        XCTAssertTrue(historyButton.exists)
         
         let tablesQuery = app.tables
-        let thisMonthSection = tablesQuery.otherElements["This Month"].staticTexts["This Month"]
-        let last3MonthsSection = tablesQuery.otherElements["Last 3 Months"].staticTexts["Last 3 Months"]
-        let moreSection = tablesQuery.otherElements["More"].staticTexts["More"]
+        let thisMonthSection = tablesQuery.otherElements["This Month"]
+        XCTAssertTrue(thisMonthSection.staticTexts["This Month"].exists)
         
-        
-        XCTAssertTrue(thisMonthSection.exists && last3MonthsSection.exists && moreSection.exists)
+        let deleteSectionButton = thisMonthSection.buttons["icon bin"]
+        if deleteSectionButton.isEnabled {
+            deleteSectionButton.tap()
+            app.alerts["Clear records for This Month?"].scrollViews.otherElements.buttons["Yes"].tap()
+            let nodataCell = app.staticTexts["No Data"]
+            XCTAssertTrue(nodataCell.exists)
+        }
         
         let backButton = app.buttons["icon leftcircle"]
         backButton.tap()
         
-        XCTAssertFalse(thisMonthSection.exists || last3MonthsSection.exists || moreSection.exists)
-       
+        XCTAssertFalse(thisMonthSection.exists)
+        XCTAssertTrue(historyButton.exists)
     }
 
     func testLaunchPerformance() {
